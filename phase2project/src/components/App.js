2014790f0803408cfdd28ca0629f900a5361c429
chris <cseiter@gmail.com>
2021-06-12 12:48:34 -0400

    simple app component imports

--- phase2project/src/components/App.js
+++ phase2project/src/components/App.js
@@ -1,9 +1,14 @@
-import '../App.css';
-import React, {useState} from "react";
+import React, { useState } from "react";
+import TaskForm from "./TaskForm";
+import TaskList from "./TaskList";
 
 function App() {
+  const [page, setPage] = useState("List");
+
   return (
     <main>
+      onChangePage={setPage}
+      {page === "Form" ? <TaskForm /> : <TaskList />}
     </main>
   );
 }
