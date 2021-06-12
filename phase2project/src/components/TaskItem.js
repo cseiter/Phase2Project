db2cda1aba50fc2df68ff13dab77146497498a3c
chris <cseiter@gmail.com>
2021-06-12 12:30:57 -0400

    create simple Task Item

---
+++ phase2project/src/components/TaskItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TaskItem({ task }) {
+  const { id, name, image, template } = task;
+
+  return (
+    <li>
+      <h4>Task {id}</h4>
+      <h5>{name}</h5>
+      {image}
+      {template}
+    </li>
+  );
+}
+
+export default TaskItem;
