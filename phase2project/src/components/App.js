238a31fdea382ba6a5585c6df8d474c7511e605f
chris <cseiter@gmail.com>
2021-06-12 21:01:32 -0400

    add buttonbar import to app component

--- phase2project/src/components/App.js
+++ phase2project/src/components/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import TaskForm from "./TaskForm";
 import TaskList from "./TaskList";
+import ButtonBar from "./ButtonBar";
 
 function App() {
   const [page, setPage] = useState("List");
 
   return (
     <main>
-      onChangePage={setPage}
+      <ButtonBar onChangePage={setPage}/>
       {page === "Form" ? <TaskForm /> : <TaskList />}
     </main>
   );
