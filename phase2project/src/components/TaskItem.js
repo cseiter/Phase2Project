4efffb45bd371a06b58b7b2d032e658a0afaeec9
chris <cseiter@gmail.com>
2021-06-13 13:17:43 -0400

    add delete function and button to taskitem component

--- phase2project/src/components/TaskItem.js
+++ phase2project/src/components/TaskItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function TaskItem({ task }) {
+function TaskItem({ task,onDeleteTask }) {
   const { id, name, image, template } = task;
 
   function selectTask(e) {
@@ -9,9 +9,19 @@ function TaskItem({ task }) {
     target.classList.toggle('selected')
   }
 
+  function handleDeleteClick() {
+    fetch(`http://localhost:3000/tasks/${id}`, {
+      method: "DELETE",
+  })
+  .then((r) => r.json())
+  .then(() => {
+    onDeleteTask(task);
+  });
+}
+
   return (
     <li>
-      <h4>Task {id}</h4>
+      <h4>Task {id}</h4><button onClick={handleDeleteClick}>Delete Task</button>
       <h5>{name}</h5>
       <br /><img src={image} alt={name} onClick={selectTask} />
       <br />{template}
