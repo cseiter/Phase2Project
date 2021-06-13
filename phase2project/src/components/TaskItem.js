c958ce63dc7ee8af465b818e66d239bd8aefd5c0
chris <cseiter@gmail.com>
2021-06-12 21:18:35 -0400

    Basic onclick event added to images

--- phase2project/src/components/TaskItem.js
+++ phase2project/src/components/TaskItem.js
@@ -3,11 +3,15 @@ import React from "react";
 function TaskItem({ task }) {
   const { id, name, image, template } = task;
 
+  function selectTask() {
+    console.log(`Task ${id}, ${name} selected`);
+  }
+
   return (
     <li>
       <h4>Task {id}</h4>
       <h5>{name}</h5>
-      <br /><img src={image} alt={name} />
+      <br /><img src={image} alt={name} onClick={selectTask} />
       <br />{template}
     </li>
   );
